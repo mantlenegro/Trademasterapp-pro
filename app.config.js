f498d9d49de902cb389586b9215e40b589852462
mantlenegro <128751657+mantlenegro@users.noreply.github.com>
2026-04-18 23:30:10 +0100

    Update and rename app.json to app.config.js

---
+++ app.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  expo: {
+    name: "Trade Academy Pro",
+    slug: "trademasterapp",
+    version: "1.0.1",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "dark",
+    backgroundColor: "#0F172A",
+    
+    ios: {
+      bundleIdentifier: "com.mantlenegro.tradeacademypro.app",
+      supportsTablet: true
+    },
+
+    android: {
+      package: "com.mantlenegro.tradeacademypro.app",
+      versionCode: 1,
+      permissions: [
+        "INTERNET",
+        "READ_EXTERNAL_STORAGE",
+        "WRITE_EXTERNAL_STORAGE"
+      ],
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#0F172A"
+      }
+    },
+
+    plugins: [
+      [
+        "react-native-google-mobile-ads",
+        {
+          "androidAppId": "ca-app-pub-9598335800956469~1158817426",
+          "iosAppId": "ca-app-pub-9598335800956469~1158817426",
+          "userTrackingUsageDescription": "This identifier will be used to deliver personalized ads to you."
+        }
+      ],
+      "expo-notifications",
+      "expo-print",
+      "expo-sharing"
+    ],
+
+    splash: {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#0F172A"
+    },
+
+    extra: {
+      eas: {
+        projectId: "1eb82123-854b-4fa8-b8e0-ed0b691e0444"
+      }
+    }
+  }
+};
